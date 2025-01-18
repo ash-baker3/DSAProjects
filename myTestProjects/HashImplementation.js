66c82282c37026202615d7f5150e66a6d4b874ba
Aysha Naushad <ayshanaushad.cn@gmail.com>
2025-01-18 08:09:32 +0000

    modified hash implementation

--- myTestProjects/HashImplementation.js
+++ myTestProjects/HashImplementation.js
@@ -57,6 +57,25 @@ class HashTable {
     console.log(valuesArray);
     return valuesArray;
   }
+
+  keysWithoutCollision() {
+    if (!this.data.length) {
+      return undefined;
+    }
+    let result = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i] && this.data[i].length) {
+        if (this.data.length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            result.push(this.data[i][j][0]);
+          }
+        } else {
+          result.push(this.data[i][0]);
+        }
+      }
+    }
+    return result;
+  }
 }
 
 const myHashTable = new HashTable(50);
@@ -67,3 +86,4 @@ myHashTable.getValue("grapes");
 myHashTable.getValue("apples");
 myHashTable.keys();
 myHashTable.values();
+myHashTable.keysWithoutCollision();
